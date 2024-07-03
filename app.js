ae7ee7e64ee689e1626f128e188db0682c20b0db
SreeTeja <gundasreeteja@gmail.com>
2024-07-03 17:17:51 -0400

    Add posting content to home feature

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let allPosts = [];
 const homeStartingContent =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
@@ -16,7 +17,34 @@ const contactContent =
   "CONTACT CONTACT Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingCont: homeStartingContent });
+  res.render("home", {
+    homeStartingCont: homeStartingContent,
+    postsCont: allPosts,
+  });
+  for (let i = 0; i < allPosts.length; i++) {
+    console.log(allPosts[i].postTitle);
+  }
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutCont: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactCont: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = {
+    postTitle: req.body.composeTitle,
+    postBody: req.body.composeTextArea,
+  };
+  allPosts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
